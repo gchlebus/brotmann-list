96033f4fcb5930d125c3e7924287c2622cd89e51
gchlebus <gchlebus@gmail.com>
2016-08-29 22:58:00 +0200

    New items can be added to the list.

--- public/app.js
+++ public/app.js
@@ -4,9 +4,14 @@ var itemCount = 0;
 
 function addItem(item) {
   $('#list').append(
-    `<li id="${item._id}" class="w3-card-4">
-      <div class="w3-container w3-xlarge">${item.text}</div>
-      <div class="w3-container">${moment().startOf('day').fromNow()}</div>
+    `<li id="${item._id}" class="w3-row">
+      <div class="w3-col s11">
+        <div class="w3-container w3-xlarge">${item.text}</div>
+        <div class="w3-container">${moment(item.createdAt).fromNow()}</div>
+      </div>
+      <div class="w3-col s1">
+	<i class="w3-closebtn w3-margin-right w3-xxlarge fa fa-times"></i>
+      </div>
     </li>`
   ); 
 }
@@ -17,11 +22,17 @@ app.configure(feathers.socketio(socket));
 app.configure(feathers.hooks());
 
 var listitems = app.service('listitems');
-listitems.on('created', function(listitem) {
+listitems.on('created', addItem);
+
+listitems.find().then(function(result) {
+  result.data.forEach(addItem);
 });
 
-addItem({
-  "_id": "blabasasd",
-  "text": "Kartofle"
+$('#add-button').on('click', function(event) {
+  var input = $('#item-text');
+  var itemText = input.val();
+  if (itemText) {
+    listitems.create({'text': itemText});
+    input.value = "";
+  }
 });
-
