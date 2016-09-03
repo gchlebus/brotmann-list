e0acb5b7235289d47131b5357cb7c9f6eaad2b44
gchlebus <gchlebus@gmail.com>
2016-09-03 21:44:01 +0200

    #7 Generate item service

--- src/services/index.js
+++ src/services/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const item = require('./item');
 const listitem = require('./listitem');
 const authentication = require('./authentication');
 const user = require('./user');
@@ -10,4 +11,5 @@ module.exports = function() {
   app.configure(authentication);
   app.configure(user);
   app.configure(listitem);
+  app.configure(item);
 };
---
+++ src/services/item/item-model.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// item-model.js - A sequelize model
+// 
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+
+const Sequelize = require('sequelize');
+
+module.exports = function(sequelize) {
+  const item = sequelize.define('items', {
+    text: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }, {
+    freezeTableName: true
+  });
+
+  item.sync();
+
+  return item;
+};
